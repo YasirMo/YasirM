e3849d24ef6e5b6be6b5e4dd1e82019f5ac8da59
YasirMo <yasirmo233@gmail.com>
2022-07-08 00:50:56 +0100

    title update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { IconButton } from "@chakra-ui/button"
 import {Flex, VStack,Heading,Spacer} from "@chakra-ui/layout"; 
 import { useColorMode } from "@chakra-ui/color-mode";
 import { FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa'
-import Contacts from'./components/contacts'
 import Profile from'./components/profile'
 import { Cover } from  './components/cover'
 
@@ -11,13 +10,16 @@ function App() {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
 
+  
   return (
     <VStack p={5}>
       <Flex w="100%">
-        <Heading ml="8" size="md" fontWeight='semibold' color="cyan.400">
+        <Heading             as="h2"
+        fontSize="5xl"
+        lineHeight="none"
+        >
        Yasir Mo        
         </Heading>
-    
       <Spacer></Spacer>
       <IconButton icon={<FaLinkedin />} isRound='true' ></IconButton>
         <IconButton ml={2} icon={<FaGithub />} isRound='true' ></IconButton>
